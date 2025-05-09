01acd139bb846cbe3d63a870337758677ca14cdc
Siri Pantzar <siri.juulia@gmail.com>
2025-05-09 13:11:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <div className="flex justify-center items-center h-screen">
+      <button 
+        className="bg-red-500 text-black font-bold py-2 px-4 rounded hover:bg-red-700">
+        I'm a button!
+      </button>
+    </div>
+
+    </>
+  )
+}
+
+export default App
