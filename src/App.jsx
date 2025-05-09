dce9b3af98ebbbbb298451eaf412235cb0af196c
Siri Pantzar <siri.juulia@gmail.com>
2025-05-09 13:17:54 +0200

    initial routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from "./pages/Home"
+import Blog from "./pages/Blog"
+import PhotoPortfolio from "./pages/PhotoPortfolio"
+import SoftwarePortfolio from "./pages/SoftwarePortfolio"
+import WorkWithMe from "./pages/WorkWithMe"
+import { Routes, Route } from 'react-router'
+
 
 function App() {
 
   return (
     <>
-
-    <div className="flex justify-center items-center h-screen">
-      <button 
-        className="bg-red-500 text-black font-bold py-2 px-4 rounded hover:bg-red-700">
-        I'm a button!
-      </button>
-    </div>
-
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/photo-portfolio" element={<PhotoPortfolio />} />
+      <Route path="/software-portfolio" element={<SoftwarePortfolio />} />
+      <Route path="/work-with-me" element={<WorkWithMe />} />
+    </Routes>
     </>
   )
 }
